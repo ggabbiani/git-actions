38262054a2b31df83345050d2ccbfb50be24d2e8
Giampiero Gabbiani <giampiero@gabbiani.org>
2026-03-30 12:23:19 +0200

    Add entry-point script, functions library, and logo example in OpenSCAD

---
+++ bin/entry-point.sh
@@ -0,0 +1,118 @@
+#!/bin/bash
+###############################################################################
+#
+#
+#
+#
+set -e # exit immediately in case of error
+ROOT="$(realpath $(dirname $0)/..)"
+BIN=$ROOT/bin
+. $ROOT/bin/functions.sh
+trap 'on_exit $? $test' EXIT
+
+help() {
+cat <<EoH
+
+$(basename $0) [-?|-h|--help] [-d|--dry-run] [-s|--silent] [-c|--camera <position>] [-p|--projection <projection>] [-r|--resolution <resolution>] [-s|--script <script name>] COMMAND PICTURE
+
+  -?|-h|--help      this help
+  -n|--native       Execute OpenSCAD script without any scaling nor check
+  -c|--camera       OpenSCAD camera position
+  -p|--projection   'ortho' or 'perspective'
+  -r|--resolution   target resolution in 'openscad' format i.e. 800x600
+  -s|--script       OpenSCAD script
+
+  COMMAND
+    'native'
+    'scaled'
+
+EoH
+exit 0
+}
+
+grep_no_rc() {
+  grep $@ || true
+}
+
+on_exit() {
+  if [ "$1" != "0" ]; then
+    color_message ${COLOR_RED} "$2 failed."
+  fi
+}
+
+VERBOSE="1"
+MODE="foundation"
+DRY="OFF"
+
+##############################################################################
+# parsing
+POSITIONALS=""
+while (( "$#" )); do
+  case "$1" in
+    '-?'|-h|--help)
+      help
+      shift
+      ;;
+    -c|--camera)
+      CAMERA="--camera=$2"
+      shift 2
+      ;;
+    -p|--projection)
+      PROJECTION="--projection=$2"
+      shift 2
+      ;;
+    -r|--resolution)
+      RESOLUTION="--resolution=$2"
+      shift 2
+      ;;
+    -s|--script)
+      SCRIPT="--ofl-script=$2"
+      shift 2
+      ;;
+    --) # end argument parsing
+      shift
+      EXTRAS="$@"
+      break
+      ;;
+    -*|--*=) # unsupported flags
+      fail 1 "Unsupported flag $1"
+      ;;
+    *) # preserve positional arguments
+      POSITIONALS="$POSITIONALS $1"
+      shift
+      ;;
+  esac
+done
+# set positional arguments in their proper place
+eval set -- "$POSITIONALS"
+
+if (( $# < 2 )); then
+  fail 2 "Missing COMMAND aand/or PICTURE."
+fi
+if [ -z "$RESOLUTION" ]; then
+    fail 3 "RESOLUTION expected."
+fi
+if [ -z "$SCRIPT" ]; then
+    fail 4 "SCRIPT expected."
+fi
+
+COMMAND=$1
+PICTURE=$2
+shift 2
+
+case "$COMMAND" in
+  'native')
+    # NOTA: crea unscaled-$PICTURE
+  	echo $BIN/make-picture.py $RESOLUTION $CAMERA $PROJECTION $SCRIPT $PICTURE
+    if (( $# > 0 )); then
+      echo mv unscaled-$PICTURE $1
+    fi
+    ;;
+  'scaled')
+    ;;
+  *)
+    fail 3 "Unsupported command '$COMMAND'"
+    ;;
+esac
+
+exit 0
---
+++ bin/make-picture.py
@@ -0,0 +1,107 @@
+#!/usr/bin/env python3
+#
+# picture creation helper for makefile usage
+#
+# This file is part of the 'OpenSCAD Foundation Library' (OFL) project.
+#
+# Copyright © 2021, Giampiero Gabbiani <giampiero@gabbiani.org>
+#
+# SPDX-License-Identifier: GPL-3.0-or-later
+#
+
+import argparse
+import dotenv
+import os
+import re
+import subprocess
+import sys
+
+import ofl
+
+from termcolor import colored, cprint
+
+def cat(f_name):
+  print(open(f_name, 'r').read())
+
+def prologue(title,case=None):
+  if (case):
+    print((colored(title+f" [{case}]: ", 'yellow')),end="",flush=True)
+  else:
+    print((colored(title+": ", 'yellow')),end="",flush=True)
+
+def hires(lowres):
+  res = [int(num)*4 for num in lowres.split('x')]
+  return str(res[0])+','+str(res[1])
+
+parser = argparse.ArgumentParser()
+parser.add_argument("-c", "--camera", help = "OpenSCAD camera position")
+parser.add_argument("-d", "--dry-run", action='store_true', help = "On screen dump only of the generated dot file")
+parser.add_argument("-p", "--projection", help = "(o)rtho or (p)erspective when exporting png")
+parser.add_argument("-t", "--temp-root", type=str, help = "Temporary directory path", choices=["/var/tmp","/tmp"],default="/tmp")
+parser.add_argument("-v", "--verbosity", type=int, help = "Increase verbosity", choices=[ofl.SILENT,ofl.ERROR,ofl.WARN,ofl.INFO,ofl.DEBUG],default=ofl.ERROR)
+parser.add_argument("--ofl-script", type=str, help="OpenSCAD script",required=True)
+parser.add_argument("-r","--resolution",type=str,help="target resolution in 'openscad' format i.e. 800x600",required=True)
+parser.add_argument("--render", action='store_true', help = "for full geometry evaluation when exporting png")
+parser.add_argument("--viewall", action='store_true', help = "adjust camera to fit object")
+parser.add_argument("--view", choices=['axes', 'crosshairs', 'edges', 'scales', 'wireframe'], help = "view options")
+parser.add_argument("--make-deps", type=str, help = "make dependency file creation")
+
+parser.add_argument("picture", type=str, help="Full target picture path")
+
+args = parser.parse_args()
+
+ofl.verbosity   = args.verbosity
+
+ofl.info("Camera        : % s" %args.camera)
+ofl.info("Projection    : % s" %args.projection)
+ofl.info("Dry run       : % s" %args.dry_run)
+ofl.info("OSCAD         : % s" %ofl.oscad)
+ofl.info("Verbosity     : % s" %args.verbosity)
+ofl.info("Resolution    : % s" %args.resolution)
+ofl.info("View          : % s" %args.view)
+
+full    = os.path.normpath(args.ofl_script.removesuffix('.scad'))
+path    = os.path.dirname(full)
+base    = os.path.basename(full)
+conf    = os.path.join(path,base+'.conf')
+json    = os.path.join(path,base+'.json')
+scad    = os.path.join(path,base+'.scad')
+
+ofl.debug("path : % s" %path)
+ofl.debug("base : % s" %base)
+ofl.debug("conf : % s" %conf)
+ofl.debug("json : % s" %json)
+ofl.debug("scad : % s" %scad)
+
+full_target = os.path.normpath(args.picture.removesuffix('.png'))
+target_path = os.path.dirname(full_target)
+target_base = os.path.basename(full_target)
+png         = os.path.join(target_path,'unscaled-'+target_base+'.png')
+
+ofl.debug("target path: % s" %target_path)
+ofl.debug("png        : % s" %png)
+
+parms = ['--imgsize',hires(args.resolution),'-o',png]
+if os.path.isfile(json):
+  parms += ['--p',json,'--P',target_base]
+if args.camera:
+  parms += ['--camera', args.camera]
+if args.projection:
+  parms += ['--projection', args.projection]
+if args.render:
+  parms += ['--render']
+if args.viewall:
+  parms += ['--viewall']
+if args.view:
+  parms += ['--view', args.view]
+if args.make_deps:
+  parms += ['--m', 'make', '--d', args.make_deps]
+echo    = os.path.join(args.temp_root,base+'.echo')
+ofl.debug("command : % s" %parms)
+ofl.debug("echo : % s" %echo)
+
+result  = ofl.openscad(scad,parms=parms,echo_f=echo,hw=True,dry_run=args.dry_run)
+if result.returncode!=0:
+  cprint(f'✝ ({result.returncode})','red')
+  cat(echo)
+  exit(result.returncode)
